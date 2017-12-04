b294e0385473be12d063397f59df39bce34949d9
Emmanuel <emmanuelbendavid@gmail.com>
2017-12-03 23:33:29 -0300

    fixup! Modularized library and created test app

--- app/src/main/java/com/lapism/searchviewapp/MainActivity.java
+++ app/src/main/java/com/lapism/searchviewapp/MainActivity.java
@@ -3,8 +3,6 @@ package com.lapism.searchviewapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    /*TODO uncomment to add options menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -37,4 +36,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
